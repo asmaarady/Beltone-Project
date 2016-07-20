c6c12ffcee2575fc84f3aa4020aaa0654e3589b4
asmaarady <asmaa.rady@gmail.com>
2016-07-20 16:31:40 +0200

    the original Global css , js before my customization

---
+++ Javascript/global js 16-7-2016.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+jQuery(document).ready(function($){
+$("#secondary_nav_top_div_4:empty").remove();
+
+$("#secondary_nav_top_div_3:empty").remove();
+
+$("#secondary_nav_top_div_2:empty").remove();
+
+$("#secondary_nav_top_div:empty").remove();
+
+});
